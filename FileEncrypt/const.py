74238405555bef2d63a6624f1ca9a0f9f3f688cd
Jordan Dorham <Otter@Jordans-MacBook-Pro.local>
2018-12-15 17:58:52 -0800

    Moved files to directory

---
+++ FileEncrypt/const.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 18:24:08 2018
+
+@author: Otter
+"""
+
+BLOCK_SIZE = 128
+MSG_LENGTH = 32
+KEY_LENGTH = 32
+IV_LENGTH = 16
